0d021148eb99bf3433062d6deb78ed1286e83bb4
Shaun Pour <scpour@gmail.com>
2021-06-10 11:35:00 -0400

    2

--- js/Bill.js
+++ js/Bill.js
@@ -1,6 +1,10 @@
-function splitBill() {
+let splitBill = () => {
     let billVal = parseInt(document.getElementById("bill-value").value);
     let partySize = parseInt(document.getElementById("party-split").value);
 
     document.getElementById("split-val").innerHTML = "Your bill will be $" + (Math.round(billVal / partySize)) + " per person.";
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("submit").addEventListener('click', splitBill);
+})
